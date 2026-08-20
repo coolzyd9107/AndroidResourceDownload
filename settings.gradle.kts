5e1dd6ce558e458328aabbe7fc7e26c7fbf9defe
coolzyd9107 <1452175643@qq.com>
2026-08-21 01:04:01 +0800

    fix build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 pluginManagement {
     repositories {
         maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/gradle-plugin")
         google()
         mavenCentral()
         gradlePluginPortal()
@@ -11,6 +12,7 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         maven("https://maven.aliyun.com/repository/google")
+        maven("https://maven.aliyun.com/repository/public")
         google()
         mavenCentral()
     }
